2c20932ab48565379fe70cf94f42860c493aecef
Thomas van Vliet <113383928+thomas-321@users.noreply.github.com>
2023-02-21 20:09:17 +0100

    snapshot: working on alligning bank image to the center

--- src/javafiles/Main.java
+++ src/javafiles/Main.java
@@ -1,76 +1,54 @@
-package javafiles;
+package javafiles;           
+// for setting an png as top left icon
 import javax.swing.*;
-import java.awt.*;
+import java.awt.Color;          // for using colors
+import java.awt.Font;           // for using fonts
+import javax.swing.border.Border;
 
-class Main {
+public class Main {
+    void createFrame(){
+        
+    }
+
     public static void main(String[] args) {
+        ImageIcon bankImage = new ImageIcon("././img/banklogo.png");
+        ImageIcon bankImageIcon = new ImageIcon("././img/banklogoRond.png");
+        //Border border = BorderFactory.createLineBorder(Color.green, 3);
+
+        // creating a label
+        // JLabel label = new JLabel();
+        // label.setText("Zie hier een bank");
+        // label.setIcon(bankImage);
+        // label.setHorizontalAlignment(JLabel.CENTER);
+        // label.setVerticalTextPosition(JLabel.NORTH);
+        // label.setFont(new Font("MV Boli", Font.PLAIN, 20));
+        // label.setBounds(0, 0, 500, 500);
+        JLabel banklogo = new JLabel();
+        banklogo.setIcon(bankImage);
+        banklogo.setHorizontalAlignment(JLabel.CENTER);
+        //banklogo.setBackground(null);
+        JPanel homescreen = new JPanel();
+        homescreen.setBounds(0, 0, 500, 500);
+        homescreen.add(banklogo);
+        //homescreen.setBackground(Color.white);
+
+
+
+
+
+
+
         
-        //Initialiseer een nieuwe JFrame en configureer de basics,
-        //dus size en de ON_CLOSE operation.
-        JFrame frame = new JFrame("Workshop frame");
-        frame.setSize(500, 500);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); 
-        
-        //Aanmaak van drie JPanels
-        JPanel flowLayoutPanel = new JPanel();
-        JPanel boxLayoutPanel = new JPanel();
-        JPanel gridLayoutPanel = new JPanel();
-        
-        //Zet voor iedere panel een aparte layout manager;
-        //FlowLayout voor links naar rechts, BoxLayout voor x-as of y-as, GridLayout voor raster.
-        //Let op dat sommige layout managers dus parameters mee moeten krijgen in hun constructors.
-        flowLayoutPanel.setLayout(new FlowLayout());
-        boxLayoutPanel.setLayout(new BoxLayout(boxLayoutPanel, BoxLayout.Y_AXIS));
-        gridLayoutPanel.setLayout(new GridLayout(3, 2));
-        
-        //Drie arrays van knoppen, 1 voor iedere panel.
-        JButton[] buttons1 = {
-        new JButton("Button1"),
-        new JButton("Button2"),
-        new JButton("Button3"),
-        new JButton("Button4"),
-        new JButton("Button5"),
-        new JButton("Button6")
-        };
-        JButton[] buttons2 = {
-        new JButton("Button7"),
-        new JButton("Button8"),
-        new JButton("Button9"),
-        new JButton("Button10"),
-        new JButton("Button11"),
-        new JButton("Button12")
-        };
-        JButton[] buttons3 = {
-        new JButton("Button13"),
-        new JButton("Button14"),
-        new JButton("Button15"),
-        new JButton("Button16"),
-        new JButton("Button17"),
-        new JButton("Button18")
-        };
-        
-        //Knoppen toevoegen aan de panels.
-        for (JButton b : buttons1){
-        flowLayoutPanel.add(b);
-        }
-        for (JButton b : buttons2){
-        boxLayoutPanel.add(b);
-        }
-        for (JButton b : buttons3){
-        gridLayoutPanel.add(b);
-        }
-        
-        //Maak een main panel aan om alle andere panels in onder te brengen, inclusief layout manager.
-        JPanel mainPanel = new JPanel();
-        mainPanel.setLayout(new BoxLayout(mainPanel, BoxLayout.Y_AXIS));
-        //Voeg de layout panels toe aan de main panel. Let op, volgorde maakt uit.
-        mainPanel.add(flowLayoutPanel);
-        mainPanel.add(boxLayoutPanel);
-        mainPanel.add(gridLayoutPanel);
-        
-        //Voeg mainpanel toe aan de JFrame en maak de JFrame visible.
-        frame.add(mainPanel);
-        frame.setVisible(true);
+        JFrame frame = new JFrame();
+        frame.setIconImage(bankImageIcon.getImage());
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setTitle("Bank GUI");
+        frame.setResizable(true);
+        frame.setVisible(true);   
+        frame.setLayout(null); 
+        frame.setSize(1900, 1000);
+        frame.getContentPane().setBackground(new Color(255, 255, 255));      // 123, 50, 250
+        //frame.add(label);
+        frame.add(homescreen);
     }
 }
-    
