b6c55451b1c78e7b1159d0f0b88a4054b7d5509f
Thomas van Vliet <113383928+thomas-321@users.noreply.github.com>
2023-03-14 15:30:57 +0100

    Now using the dedicated GUI class for the gui
    De gui heeft nu zijn eigen class, zodat deze gescheiden kan worden van de
    nog te programmeren logica

--- src/javafiles/Main.java
+++ src/javafiles/Main.java
@@ -1,46 +1,17 @@
 package javafiles;           
-// for setting an png as top left icon
-import javax.swing.*;
-import java.awt.Color;          // for using colors
-import java.awt.Font;           // for using fonts
-// import javax.swing.border.Border;
+
 
 public class Main {
-    void createFrame(){
-        
-    }
+
 
     public static void main(String[] args) {
-        ImageIcon bankImage = new ImageIcon("././img/banklogo.png");
-        ImageIcon bankImageIcon = new ImageIcon("././img/banklogoRond.png");
+        GUI scherm = new GUI();
+        scherm.startup();
+        scherm.updateTime();
 
-        JLabel homescreenlabel1 = new JLabel();
-        homescreenlabel1.setText("Voer uw pas in");
-        homescreenlabel1.setFont(new Font(null, Font.PLAIN, 35));
-        homescreenlabel1.setIcon(bankImage);
-        homescreenlabel1.setVerticalTextPosition(JLabel.BOTTOM);
-        homescreenlabel1.setHorizontalTextPosition(JLabel.CENTER);
-
-        // JLabel homescreenlabel2 = new JLabel();
-        // code to be added for the date and time
-
-        JPanel homescreen = new JPanel();
-        homescreen.setBounds(500, 100, 500, 500);
-        homescreen.add(homescreenlabel1);
-        homescreen.setBackground(Color.white);
-        
-        // Frame waar de GUI in draait
-        JFrame frame = new JFrame();
-        frame.setIconImage(bankImageIcon.getImage());
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setTitle("Bank GUI");
-        frame.setResizable(true);
-        frame.setVisible(true);   
-        frame.setLayout(null); 
-        frame.setSize(1900, 1000);
-        frame.getContentPane().setBackground(new Color(255, 255, 255));      // 123, 50, 250
-        //frame.add(label);
-        frame.add(homescreen);
+        while (true){
+            scherm.updateTime();
+        }
     }
 }
 
