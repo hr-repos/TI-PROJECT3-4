cb51a2d9b2aedc3829bed764f04b019c6b409589
Thomas van Vliet <113383928+thomas-321@users.noreply.github.com>
2023-03-14 16:41:43 +0100

    Code cleanup

--- src/javafiles/Main.java
+++ src/javafiles/Main.java
@@ -1,33 +1,14 @@
 package javafiles;           
-
-
 public class Main {
-
 
     public static void main(String[] args) {
         GUI scherm = new GUI();
         scherm.startup();
         scherm.updateTime();
-
-        while (true){
+        // scherm.setCardDetectedScreen();      // tijdelijk om dit scherm te programmeren
+        while (true){   // main loop
             scherm.updateTime();
+            
         }
     }
 }
-
-
-
-
-
-
-
-        //Border border = BorderFactory.createLineBorder(Color.green, 3);
-
-        // creating a label
-        // JLabel label = new JLabel();
-        // label.setText("Zie hier een bank");
-        // label.setIcon(bankImage);
-        // label.setHorizontalAlignment(JLabel.CENTER);
-        // label.setVerticalTextPosition(JLabel.NORTH);
-        // label.setFont(new Font("MV Boli", Font.PLAIN, 20));
-        // label.setBounds(0, 0, 500, 500);
