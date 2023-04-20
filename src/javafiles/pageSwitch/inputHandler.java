dfab0ccd1d1464147ef02f96aea7851ac9099081
TigoG <tigogoes11@gmail.com>
2023-04-20 13:24:37 +0200

    buttons work now

---
+++ src/javafiles/pageSwitch/inputHandler.java
@@ -0,0 +1,188 @@
+package javafiles.pageSwitch;
+
+import java.awt.EventQueue;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+import javax.swing.SwingUtilities;
+
+import com.fazecast.jSerialComm.SerialPort;
+import javafiles.GUI;
+
+public class inputHandler {
+  private SerialPort serialPort;
+  private BufferedReader input;
+
+  private final GUI scherm;
+  private int count;
+  private int triesLeft = 3;
+  private int wrongTries;
+
+  public inputHandler(GUI scherm) {
+    this.scherm = scherm;
+  }
+
+  //code to initialize java and arduino
+  public void initialize() {
+    serialPort = SerialPort.getCommPort("COM8"); // Personal COM 6 for uno and 8 for mega
+    serialPort.setBaudRate(9600);
+    serialPort.openPort();
+    serialPort.setComPortTimeouts(SerialPort.TIMEOUT_READ_SEMI_BLOCKING, 500, 0); // set timeout to 500ms
+    input = new BufferedReader(new InputStreamReader(serialPort.getInputStream()));
+  }
+
+  public void readData() {
+    while (true) {
+        try {
+            // Check if there are bytes available to be read from the input buffer
+            if (serialPort.bytesAvailable() > 0) {
+                String inputLine = input.readLine();
+                System.out.println(inputLine);
+                //checks what screen is currently visible
+                if (scherm.isCurrentScreen(0)) {
+                    currentScreenZero(inputLine);
+                } else if (scherm.isCurrentScreen(1)) {
+                    currentScreenOne(inputLine);
+                } else if (scherm.isCurrentScreen(2)){
+                    currentScreenTwo(inputLine);
+                }   else if (scherm.isCurrentScreen(3)){
+                    currentScreenThree(inputLine);
+                }  else if (scherm.isCurrentScreen(4)){
+                    currentScreenFour(inputLine);
+                } else if (scherm.isCurrentScreen(5)){
+                    currentScreenFive(inputLine);
+                } else if (scherm.isCurrentScreen(6)){
+                    currentScreenSix(inputLine);
+                } else if (scherm.isCurrentScreen(7)){
+                    currentScreenSeven(inputLine);}
+            }
+                else {}
+            
+        } catch (Exception e) {
+            System.err.println(e.toString());
+        }
+    }
+}
+
+public void updateSetPinNumbersEntered(int count){
+  scherm.setPinNumbersEntered(count);
+  SwingUtilities.invokeLater(() -> scherm.setCardDetectedScreen());
+}
+
+//code for homeScreen
+public void currentScreenZero(String inputLine) {
+  if (inputLine.equals("pass found")) {
+      updateSetPinNumbersEntered(0); 
+  }
+  else{}
+}
+
+//code for pinScreen
+public void currentScreenOne(String inputLine) {
+  //checks what the input is
+  if (inputLine.equals("plus")) {
+      if (count < 5) {
+          count++;
+          updateSetPinNumbersEntered(count);
+      }
+  } else if (inputLine.equals("minus")) {
+      if (count > 0) {
+          count--;
+          updateSetPinNumbersEntered(count);
+      }
+  } else if (inputLine.equals("wrong")) {
+      if (triesLeft > 1) {
+          count = 0;
+          triesLeft--;
+          wrongTries++;
+          System.out.println("Je hebt nog " + triesLeft + " pogingen");
+          scherm.attempsLeft(wrongTries);
+          updateSetPinNumbersEntered(count);
+      } else {
+          SwingUtilities.invokeLater(() -> scherm.setHomeScreen());
+
+      }   
+  } else if (inputLine.equals("correct")) {
+    //hier kan het volgende scherm
+      SwingUtilities.invokeLater(() -> scherm.setLoggedInScreen());
+  }
+}
+
+//Options
+public void currentScreenTwo(String inputLine){
+    if (inputLine.equals("a")){
+        SwingUtilities.invokeLater(() -> scherm.setCheckSaldoScreen());
+    }
+    else if (inputLine.equals("b")){
+        SwingUtilities.invokeLater(() -> scherm.setHomeScreen());
+    }
+    else if (inputLine.equals("c")){
+        SwingUtilities.invokeLater(() -> scherm.setWithdrawScreen());
+    }
+    else if (inputLine.equals("d")){
+        SwingUtilities.invokeLater(() -> scherm.setFastMoneyScreen());
+    }
+}
+
+//Quick option
+public void currentScreenThree(String inputLine){
+    if (inputLine.equals("a")){
+        SwingUtilities.invokeLater(() -> scherm.setReceiptScreen());
+    }
+    else if (inputLine.equals("b")){
+        SwingUtilities.invokeLater(() -> scherm.setLoggedInScreen());
+    }
+    else if (inputLine.equals("c")){
+        SwingUtilities.invokeLater(() -> scherm.setReceiptScreen());
+    }
+    else if (inputLine.equals("d")){
+        SwingUtilities.invokeLater(() -> scherm.setReceiptScreen());
+    }
+}
+
+//Saldo check
+public void currentScreenFour(String inputLine){
+    if (inputLine != null){
+        SwingUtilities.invokeLater(() -> scherm.setLoggedInScreen());
+    }
+    else{}}
+
+//Withdraw
+public void currentScreenFive(String inputLine){
+    if (inputLine.equals("d")){
+        SwingUtilities.invokeLater(() -> scherm.setReceiptScreen());
+    }
+    else if (inputLine.equals("b")){
+        SwingUtilities.invokeLater(() -> scherm.setLoggedInScreen());
+    }
+}
+
+public void currentScreenSix(String inputLine){
+    if (inputLine.equals("b")){
+        SwingUtilities.invokeLater(() -> scherm.setGoodbyeScreen());
+    }
+    else if (inputLine.equals("d")){
+        SwingUtilities.invokeLater(() -> scherm.setGoodbyeScreen());
+    }
+}
+
+public void currentScreenSeven(String inputLine){
+    if (inputLine != null){
+        SwingUtilities.invokeLater(() -> scherm.setHomeScreen());
+    }
+    else{}
+}
+
+  public static void main(String[] args) {
+    GUI scherm = new GUI();
+    EventQueue.invokeLater(() -> {
+        scherm.startup();
+        scherm.updateTime();
+    });
+    
+    inputHandler listener = new inputHandler(scherm);
+    listener.initialize();
+    Thread readDataThread = new Thread(() -> listener.readData());
+    readDataThread.start();
+  }
+}
