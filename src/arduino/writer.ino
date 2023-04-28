06e3a587160e009c7aff001e4ce129785f47dcd8
TigoG <tigogoes11@gmail.com>
2023-04-28 12:28:57 +0200

    write data to blocks

--- src/arduino/writer.ino
+++ src/arduino/writer.ino
@@ -1,46 +1,141 @@
-#include <SPI.h>
-#include <MFRC522.h>
-#include <ArduinoJson.h>
 
-// Define the RFID reader pins
-#define SS_PIN 9
-#define RST_PIN 8
+/******************************************
+        PURPOSE:  Learn to use the MF522-AN RFID card reader 
+  Created by      Rudy Schlaf for www.makecourse.com
+  DATE:   2/2014
+*******************************************/
 
-// Create an instance of the RFID reader
-MFRC522 mfrc522(SS_PIN, RST_PIN);
+/*
+ * This sketch uses the MFRC522 Library to use ARDUINO RFID MODULE KIT 13.56 MHZ WITH TAGS SPI W AND R BY COOQROBOT.
+ * The library file MFRC522.h has a wealth of useful info. Please read it.
+ * The functions are documented in MFRC522.cpp.
+ *
+ * Based on code Dr.Leong   ( WWW.B2CQSHOP.COM )
+ * Created by Miguel Balboa (circuitito.com), Jan, 2012.
+ * Rewritten by Søren Thing Andersen (access.thing.dk), fall of 2013 (Translation to English, refactored, comments, anti collision, cascade levels.)
+ * 
+ * This library has been released into the public domain.
+*/
+
+
+#include <SPI.h>//include the SPI bus library
+#include <MFRC522.h>//include the RFID reader library
+
+
+#define SS_PIN 9  //slave select pin
+#define RST_PIN 8  //reset pin
+MFRC522 mfrc522(SS_PIN, RST_PIN);        // instatiate a MFRC522 reader object.
+MFRC522::MIFARE_Key key;//create a MIFARE_Key struct named 'key', which will hold the card information
+
 
 void setup() {
-  Serial.begin(9600);
-  while (!Serial);  // wait for serial port to connect
+        Serial.begin(9600);        // Initialize serial communications with the PC
+        SPI.begin();               // Init SPI bus
+        mfrc522.PCD_Init();        // Init MFRC522 card (in case you wonder what PCD means: proximity coupling device)
+        Serial.println("Scan a MIFARE Classic card");
+        
+        // Prepare the security key for the read and write functions - all six key bytes are set to 0xFF at chip delivery from the factory.
+        // Since the cards in the kit are new and the keys were never defined, they are 0xFF
+        // if we had a card that was programmed by someone else, we would need to know the key to be able to access it. This key would then need to be stored in 'key' instead.
+ 
+        for (byte i = 0; i < 6; i++) {
+                key.keyByte[i] = 0xFF;//keyByte is defined in the "MIFARE_Key" 'struct' definition in the .h file of the library
+        }
 
-  SPI.begin();      // Init SPI bus
-  mfrc522.PCD_Init(); // Init MFRC522
-  Serial.println("Scan a RFID tag");
 }
 
-void loop() {
-  // Wait for a RFID tag to be detected
-  if (!mfrc522.PICC_IsNewCardPresent() || !mfrc522.PICC_ReadCardSerial()) {
-    return;
+//block 4 for country, 5 for bank and 6 for iban
+
+int block=4;//this is the block number we will write into and then read. Do not write into 'sector trailer' block, since this can make the block unusable.
+
+byte blockcontent[16] = {"\"Ctry\":\"LU\","};
+//byte blockcontent[16] = {"\"Bank\":\"BK\""};                          
+//byte blockcontent[16] = {"LU01BK000000001"};
+//byte blockcontent[16] = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};//all zeros. This can be used to delete a block.
+byte readbackblock[18];//This array is used for reading out a block. The MIFARE_Read method requires a buffer that is at least 18 bytes to hold the 16 bytes of a block.
+
+void loop()
+{
+
+        
+    // Look for new cards (in case you wonder what PICC means: proximity integrated circuit card)
+  if ( ! mfrc522.PICC_IsNewCardPresent()) {//if PICC_IsNewCardPresent returns 1, a new card has been found and we continue
+    return;//if it did not find a new card is returns a '0' and we return to the start of the loop
   }
 
-  // Define the data as a JSON object
-  StaticJsonDocument<128> jsonDoc;
-  jsonDoc["Ctry"] = "LU";
-  jsonDoc["Bank"] = "BK";
-  jsonDoc["acctNo"] = "LU01BK000000001";
-
-  // Serialize the JSON object to a byte array
-  byte data[16];
-  serializeJson(jsonDoc, data, sizeof(data));
-
-  // Write the data to block 1
-  MFRC522::StatusCode status = mfrc522.MIFARE_Write(1, data, sizeof(data));
-  if (status == MFRC522::STATUS_OK) {
-    Serial.println("Data written to tag");
-  } else {
-    Serial.println("Write failed");
-    Serial.print("Error code: ");
-    Serial.println(status);
+  // Select one of the cards
+  if ( ! mfrc522.PICC_ReadCardSerial()) {//if PICC_ReadCardSerial returns 1, the "uid" struct (see MFRC522.h lines 238-45)) contains the ID of the read card.
+    return;//if it returns a '0' something went wrong and we return to the start of the loop
   }
+
+         
+         Serial.println("card selected");
+         
+         /*****************************************writing and reading a block on the card**********************************************************************/
+         
+         writeBlock(block, blockcontent);//the blockcontent array is written into the card block
+
+
+         
+         readBlock(block, readbackblock);//read the block back
+         Serial.print("read block: ");
+         for (int j=0 ; j<16 ; j++)//print the block contents
+         {
+           Serial.write (readbackblock[j]);//Serial.write() transmits the ASCII numbers as human readable characters to serial monitor
+         }
+         Serial.println("");
+         
+ 
+}
+
+int writeBlock(int blockNumber, byte arrayAddress[]) 
+{
+  //this makes sure that we only write into data blocks. Every 4th block is a trailer block for the access/security info.
+  int largestModulo4Number=blockNumber/4*4;
+  int trailerBlock=largestModulo4Number+3;//determine trailer block for the sector
+  if (blockNumber > 2 && (blockNumber+1)%4 == 0){Serial.print(blockNumber);Serial.println(" is a trailer block:");return 2;}//block number is a trailer block (modulo 4); quit and send error code 2
+  Serial.print(blockNumber);
+  Serial.println(" is a data block:");
+  
+  /*****************************************authentication of the desired block for access***********************************************************/
+  byte status = mfrc522.PCD_Authenticate(MFRC522::PICC_CMD_MF_AUTH_KEY_A, trailerBlock, &key, &(mfrc522.uid));
+
+  if (status != MFRC522::STATUS_OK) {
+         Serial.print("PCD_Authenticate() failed: ");
+         Serial.println(mfrc522.GetStatusCodeName(status));
+         return 3;//return "3" as error message
+  }
+        
+  status = mfrc522.MIFARE_Write(blockNumber, arrayAddress, 16);//valueBlockA is the block number, MIFARE_Write(block number (0-15), byte array containing 16 values, number of bytes in block (=16))
+  //status = mfrc522.MIFARE_Write(9, value1Block, 16);
+  if (status != MFRC522::STATUS_OK) {
+           Serial.print("MIFARE_Write() failed: ");
+           Serial.println(mfrc522.GetStatusCodeName(status));
+           return 4;//return "4" as error message
+  }
+  Serial.println("block was written");
+}
+
+
+int readBlock(int blockNumber, byte arrayAddress[]) 
+{
+  int largestModulo4Number=blockNumber/4*4;
+  int trailerBlock=largestModulo4Number+3;//determine trailer block for the sector
+
+  /*****************************************authentication of the desired block for access***********************************************************/
+  byte status = mfrc522.PCD_Authenticate(MFRC522::PICC_CMD_MF_AUTH_KEY_A, trailerBlock, &key, &(mfrc522.uid));
+  if (status != MFRC522::STATUS_OK) {
+         Serial.print("PCD_Authenticate() failed (read): ");
+         Serial.println(mfrc522.GetStatusCodeName(status));
+         return 3;//return "3" as error message
+  }
+        
+  byte buffersize = 18;//we need to define a variable with the read buffer size, since the MIFARE_Read method below needs a pointer to the variable that contains the size... 
+  status = mfrc522.MIFARE_Read(blockNumber, arrayAddress, &buffersize);//&buffersize is a pointer to the buffersize variable; MIFARE_Read requires a pointer instead of just a number
+  if (status != MFRC522::STATUS_OK) {
+          Serial.print("MIFARE_read() failed: ");
+          Serial.println(mfrc522.GetStatusCodeName(status));
+          return 4;//return "4" as error message
+  }
+  Serial.println("block was read");
 }
