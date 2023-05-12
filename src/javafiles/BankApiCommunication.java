eade0e61a925b9f30a8aa975262ab7a1c45c662c
Thomas van Vliet <113383928+thomas-321@users.noreply.github.com>
2023-05-12 13:22:37 +0200

    Added error handling

--- src/javafiles/BankApiCommunication.java
+++ src/javafiles/BankApiCommunication.java
@@ -1,7 +1,6 @@
 
 package javafiles;
 
-import java.awt.geom.RoundRectangle2D.Float;
 import java.io.BufferedReader;
 import java.io.DataOutputStream;
 import java.io.InputStreamReader;
@@ -38,12 +37,29 @@ public class BankApiCommunication {
         return text.equals("LUX01BANK");
     }
 
+    public boolean checkIfError(String inpuString){
+        int offset = Math.min(3, inpuString.length());
+        String text = inpuString.substring(0, offset);
+        return text.equals("LU: ");
+    }
+
     // true goedgekeurd, // false niet goedgekeurd
-    Double withdraw(String toCtry, String toBank, String acctNo, String pin, int amount) {
+    Double getBalanceFromJsonString(String apiResponse) {
         try {
-            String apiResponse = postApiRequest(toCtry, toBank, acctNo, pin, amount);
             JsonObject jsonObject = gson.fromJson(apiResponse, JsonObject.class);
-            // System.out.println(jsonObject);
+            return jsonObject.getAsJsonObject("body").get("balance").getAsDouble();
+
+        } catch (Exception e) {
+            System.out.println(e);
+            return null;
+        }
+
+    }
+
+    Double getBalanceFromJson(String apiResponse) {
+        try {
+
+            JsonObject jsonObject = gson.fromJson(apiResponse, JsonObject.class);
             if (jsonObject.getAsJsonObject("body").get("succes").getAsBoolean() != true){
                 return null;
             } else {
@@ -54,20 +70,6 @@ public class BankApiCommunication {
             return null;
         }
 
-    }
-
-    // return het bedrag dat op de rekening staat
-    public Double getBalance(String toCtry, String toBank, String acctNo, String pin){
-        try {
-            String apiResponse = postApiRequest(toCtry, toBank, acctNo, pin, 0);
-            JsonObject jsonObject = gson.fromJson(apiResponse, JsonObject.class);
-            // System.out.println(jsonObject);
-
-            return jsonObject.getAsJsonObject("body").get("balance").getAsDouble();
-        } catch (Exception e) {
-            System.out.println(e);
-            return -1.0;
-        }
     }
 
     private String createJsonPacket(String toCtry, String toBank, String acctNo, String pin, int amount){
@@ -92,9 +94,11 @@ public class BankApiCommunication {
         return gsonPretty.toJson(payload);
     }
 
-    private String postApiRequest(String toCtry, String toBank, String acctNo, String pin, int amount){
+    public String postApiRequest(String toCtry, String toBank, String acctNo, String pin, int amount){
         String jsonRequestString = createJsonPacket(toCtry, toBank, acctNo, pin, amount);
+        String jsonAnswer = "";
         URL url;
+        HttpURLConnection conn;
         try {
             if (checkIfLocalAccount(acctNo) && amount == 0){
                 url = new URL("http://localhost:8443/balance");
@@ -109,31 +113,44 @@ public class BankApiCommunication {
                 return "";
             }
             
-            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn = (HttpURLConnection) url.openConnection();
             conn.setRequestMethod("POST");
             conn.setDoOutput(true);
             conn.setRequestProperty("Content-Type", "application/json");
             conn.setRequestProperty("User-Agent", "Mozilla/5.0");
+            
             
             // send json payload
             try (DataOutputStream dos = new DataOutputStream(conn.getOutputStream())) {
                 dos.writeBytes(jsonRequestString);
             }
             
-            // Read Response from API
-            try (BufferedReader bf = new BufferedReader(new InputStreamReader(conn.getInputStream()))) {
-                String line;
-                while ((line = bf.readLine()) != null) {
-                    System.out.println(line);
-                    return line;
+            if (conn.getResponseCode() >= 400){
+                try (BufferedReader bf = new BufferedReader(new InputStreamReader(conn.getErrorStream()))) {
+                    StringBuilder errorMessage = new StringBuilder();
+                    String line;
+                    while ((line = bf.readLine()) != null) {
+                        errorMessage.append(line);
+                    }
+                    System.out.println("Error Response from API: " + errorMessage.toString());
+                    return errorMessage.toString();
                 }
-                return "";
-                
+            }
+            else {
+                // Read Response from API
+                try (BufferedReader bf = new BufferedReader(new InputStreamReader(conn.getInputStream()))) {
+                    while ((jsonAnswer = bf.readLine()) != null) {
+                        System.out.println("jsonAnswer: " + jsonAnswer);
+                        return jsonAnswer;
+                    }
+                    return "";
+                    
+                }
             }
         } catch (Exception e) {
             System.out.println("error in function postApiRequest");
-            System.out.println(e);
-            return "";
+            System.out.println(e.getMessage());
+            return jsonAnswer;
         }
     }
 }
